48bc27450f45f9c071d1d4a5c35cc67089bb8e9b
zoenirina <zoenirinamarieviviane@gmail.com>
2025-08-21 09:39:17 +0500

    Creation de la classe MoneyBag

---
+++ src/fr/emse/test/MoneyBag.java
@@ -0,0 +1,34 @@
+package fr.emse.test;
+
+import java.util.Vector; 
+
+public class MoneyBag {
+	private Vector<Money> fMonies = new Vector<Money>(); 
+	
+	MoneyBag(Money m1, Money m2) { 
+		appendMoney(m1); 
+		appendMoney(m2); 
+	} 
+	
+	MoneyBag(Money bag[]) { 
+		for (int i = 0; i < bag.length; i++) 
+		appendMoney(bag[i]); 
+	}
+	
+	private void appendMoney(Money m) { 
+		if (fMonies.isEmpty()) { 
+			fMonies.add(m); 
+		} else { 
+			int i = 0; 
+			while ((i < fMonies.size()) 
+			&& (!(fMonies.get(i).currency().equals(m.currency())))) 
+			i++; 
+			if (i >= fMonies.size()) { 
+				fMonies.add(m); 
+			} else { 
+				fMonies.set(i, new Money(fMonies.get(i).amount() + 
+				m.amount(), m.currency())); 
+			}
+		}
+	}
+}
