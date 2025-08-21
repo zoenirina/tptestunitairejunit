1595e4b8a81edd3b497bbb8d5fa88682f8363d3b
zoenirina <zoenirinamarieviviane@gmail.com>
2025-08-21 09:32:07 +0500

    Utilisation de l'annotation @Before pour éviter la duplication  et test passé avec succès

--- src/fr/emse/test/MoneyTest.java
+++ src/fr/emse/test/MoneyTest.java
@@ -2,14 +2,21 @@ package fr.emse.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class MoneyTest {
-
+    private Money m12CHF;
+    private Money m14CHF;
+    
+	@Before
+    public void setUp() {
+        m12CHF = new Money(12, "CHF");
+        m14CHF = new Money(14, "CHF");
+    }
+	 
 	@Test 
 	public void testSimpleAdd() { 
-		Money m12CHF = new Money(12, "CHF"); // création de données 
-		Money m14CHF = new Money(14, "CHF"); 
 		Money expected = new Money(26, "CHF"); 
 		Money result = m12CHF.add(m14CHF); // exécution de la méthode testée 
 		assertTrue(expected.equals(result)); // comparaison @Test
@@ -17,8 +24,6 @@ public class MoneyTest {
 	
 	@Test 
 	public void testEquals() { 
-		Money m12CHF= new Money(12, "CHF"); 
-		Money m14CHF= new Money(14, "CHF"); 
 		assertTrue(!m12CHF.equals(null)); 
 		assertEquals(m12CHF, m12CHF); 
 		assertEquals(m12CHF, new Money(12, "CHF")); 
