d896d63650f6c34ce13960616d6d711f411326ab
zoenirina <zoenirinamarieviviane@gmail.com>
2025-08-21 09:23:36 +0500

    Surcharge de la méthode equals de la classe Money et test passé avec succès

--- src/fr/emse/test/Money.java
+++ src/fr/emse/test/Money.java
@@ -20,4 +20,12 @@ class Money{
 	public Money add(Money m) { 
 		return new Money(amount() + m.amount(), currency()); 
 	}
+	
+	@Override
+	public boolean equals(Object obj) {
+	    if (this == obj) return true;
+	    if (obj == null || getClass() != obj.getClass()) return false;
+	    Money money = (Money) obj;
+	    return fAmount == money.fAmount && fCurrency.equals(money.fCurrency);
+	}
 }
