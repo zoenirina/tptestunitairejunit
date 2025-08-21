cbccb91e8f675a8a4b7d17f967d26ffe7bb1591a
zoenirina <zoenirinamarieviviane@gmail.com>
2025-08-21 09:14:14 +0500

    Test échoué sur la fonction testSimpleAdd, création de la fonction testEquals puis ECHEC

--- src/fr/emse/test/MoneyTest.java
+++ src/fr/emse/test/MoneyTest.java
@@ -15,4 +15,14 @@ public class MoneyTest {
 		assertTrue(expected.equals(result)); // comparaison @Test
 	}
 	
+	@Test 
+	public void testEquals() { 
+		Money m12CHF= new Money(12, "CHF"); 
+		Money m14CHF= new Money(14, "CHF"); 
+		assertTrue(!m12CHF.equals(null)); 
+		assertEquals(m12CHF, m12CHF); 
+		assertEquals(m12CHF, new Money(12, "CHF")); 
+		assertTrue(!m12CHF.equals(m14CHF)); 
+	}
+	
 }
